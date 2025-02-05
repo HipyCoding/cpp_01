e319968e01eb4ddac79eb55c44d7ca7d62a56da8
Christian Andresen <christian@Christians-MacBook-Pro.local>
2025-02-05 04:48:08 +0100

    ex02 works correctly

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,10 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: candrese <candrese@student.42.fr>          +#+  +:+       +#+        */
+/*   By: christian <christian@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 23:31:19 by candrese          #+#    #+#             */
-/*   Updated: 2025/02/03 23:31:20 by candrese         ###   ########.fr       */
+/*   Updated: 2025/02/05 04:47:00 by christian        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
+
+	std::cout << "string: " << &str << std::endl;
+	std::cout << "stringPTR: " << stringPTR << std::endl;
+	std::cout << "stringREF: " << &stringREF << std::endl;
+
+	std::cout << "\nstring: " << str << std::endl;
+	std::cout << "stringPTR: " << *stringPTR << std::endl;
+	std::cout << "stringREF: " << stringREF << std::endl;
+
+	return 0;
+}
